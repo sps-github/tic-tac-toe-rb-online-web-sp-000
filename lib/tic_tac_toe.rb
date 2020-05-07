6d4e495163ac1c6204620895e307e4e212855b04
Sarah Peters <sarahpieters15@gmail.com>
2020-05-07 16:03:35 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -71,9 +71,9 @@ def won?(board)
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
 
-    if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
-      return win_combination
-    end
+    # if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
+    #   return win_combination
+    # end
   end
   return false
 end
